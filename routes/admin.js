cff9312f48e85292579079a1035aa02b3f3683cd
Limmanuel <limmanuellopez@gmail.com>
2017-05-31 17:38:33 +0800

    Project Report added hours

--- routes/admin.js
+++ routes/admin.js
@@ -198,8 +198,7 @@ router.get('/login', function(req,res,next){
 								var time = moment().format('HH:MM:ss');
 								if(time >= sched){
 									Time.getTimeLogsByUserAndDate(userid, moment().format('MM-DD-YYYY'), function(err, user){
-										if(user.timein[0]){
-											if(user.timein[0].timein !== 'N/A'){
+											if(user.timein[0].timein){} else{
 												let query = {user_id: userid, date: moment().format('MM-DD-YYYY'), 'status.status': 'Absent'};
 												var timein = {
 													timein: "N/A",
